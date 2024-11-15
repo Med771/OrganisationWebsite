ed2dfe453d1a968e6eaabfe2088e7141155361c8
Med771 <mm7737333@gmail.com>
2024-11-15 07:17:28 +0300

    Begin

---
+++ src/main/java/com/example/organisationwebsite/OrganisationWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.organisationwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganisationWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrganisationWebsiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/organisationwebsite/OrganisationWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.organisationwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganisationWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
